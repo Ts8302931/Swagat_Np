146c2036990a1a4bcbae2a911192a684886914d6
Ts8302931 <130143853+Ts8302931@users.noreply.github.com>
2025-09-28 22:06:04 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Swagat NP',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Swagat NP App"),
+      ),
+      body: const Center(
+        child: Text(
+          "Hello! Your app is ready 🚀",
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
